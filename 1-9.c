df91d36ce205006b192b3cb32e39f4ba610b9747
pavan <pavan0654@gmail.com>
2016-04-25 16:00:08 +0530

    Create 1-9.c

---
+++ 1-9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int c;
+    while((c= getchar())!=EOF){
+        if(c ==' '){
+            putchar(c);
+            while((c=getchar())==' '){
+                //putchar(c);
+                //break;
+            }
+        }
+          if (c != ' ') {
+            putchar(c);
+            
+        }
+    
+    }
+}
